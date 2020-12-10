453e9a61637ddeb1defc24f23efbcc0e886b2f81
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2020-12-10 15:46:38 +0800

    Add event listener for R-P-S

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+
+rock_div.addEventListener('click', function() {
+  console.log("Hey, you clicked on Rock");
+})
+paper_div.addEventListener('click', function() {
+  console.log("Hey, you clicked on Paper");
+})
+scissors_div.addEventListener('click', function() {
+  console.log("Hey, you clicked on Scissors");
+})
